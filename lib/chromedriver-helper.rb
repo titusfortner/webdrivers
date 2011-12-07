64dee791eb39a9d6b5c09f77ec633136d36a3fce
Mike Dalessio <mike@csa.net>
2011-12-06 20:58:17 -0500

    First commit.

---
+++ lib/chromedriver-helper.rb
@@ -0,0 +1,7 @@
+require "chromedriver-helper/version"
+
+module Chromedriver
+  module Helper
+    # Your code goes here...
+  end
+end
---
+++ lib/chromedriver-helper/version.rb
@@ -0,0 +1,5 @@
+module Chromedriver
+  module Helper
+    VERSION = "0.0.1"
+  end
+end
