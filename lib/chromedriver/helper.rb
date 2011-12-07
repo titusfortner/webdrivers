64a0dcb0af722cfa3da7089602a4d6d5d1006d15
Mike Dalessio <mike@csa.net>
2011-12-06 23:31:00 -0500

    Chromedriver::Helper birthed, along with executable script "chromedriver".

--- lib/chromedriver/helper.rb
+++ lib/chromedriver/helper.rb
@@ -1,8 +1,59 @@
 require "chromedriver/helper/version"
 require "chromedriver/helper/google_code_parser"
+require 'fileutils'
+require 'open-uri'
 
 module Chromedriver
   class Helper
+    DOWNLOAD_URL = "http://code.google.com/p/chromium/downloads/list"
 
+    def run *args
+      download
+      exec binary_path, *args
+    end
+
+    def download
+      return if File.exists? binary_path
+      url = download_url
+      filename = File.basename url
+      Dir.chdir platform_install_dir do
+        system("wget -c -O #{filename} #{url}") || system("curl -C - -o #{filename} #{url}")
+        raise "Could not download #{url}" unless File.exists? filename
+        system "unzip #{filename}"
+      end
+      raise "Could not unzip #{filename} to get #{binary_path}" unless File.exists? binary_path
+    end
+
+    def download_url
+      downloads = GoogleCodeParser.new(open(DOWNLOAD_URL)).downloads
+      url = downloads.grep(/chromedriver_#{platform}_.*\.zip/).first
+      url = "http:#{url}" if url !~ /^http/
+      url
+    end
+
+    def binary_path
+      File.join platform_install_dir, "chromedriver"
+    end
+
+    def platform_install_dir
+      dir = File.join install_dir, platform
+      FileUtils.mkdir_p dir
+      dir
+    end
+
+    def install_dir
+      dir = File.expand_path File.join(ENV['HOME'], ".chromedriver-helper")
+      FileUtils.mkdir_p dir
+      dir
+    end
+
+    def platform
+      case RUBY_PLATFORM
+      when /64.*linux|linux.*64/ then "linux64"
+      when /linux/ then "linux32"
+      when /darwin/ then "mac"
+      else "win"
+      end
+    end
   end
 end
