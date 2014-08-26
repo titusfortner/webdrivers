bbff892148d59466cf39a8932e69e40b95f7b4ab
Mike Dalessio <mike@csa.net>
2014-08-26 06:08:31 -0400

    Adding tests around the windows binary_path support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 require "rspec"
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib/chromedriver/helper/google_code_parser"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib/chromedriver/helper"))
