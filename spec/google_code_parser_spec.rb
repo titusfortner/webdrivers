ff1633f9376de7859bd1142c787a96c6f3087075
Mike Dalessio <mike.dalessio@gmail.com>
2015-01-23 15:31:55 -0500

    Adding test coverage for the version sorting.
    Related to #15

--- spec/google_code_parser_spec.rb
+++ spec/google_code_parser_spec.rb
@@ -23,6 +23,20 @@ describe Chromedriver::Helper::GoogleCodeParser do
     it "returns the last URL for the platform" do
       expect(parser.newest_download).to eq "http://chromedriver.storage.googleapis.com/2.4/chromedriver_mac32.zip"
     end
+
+    context "out-of-order versions" do
+      before do
+        allow(parser).to receive(:downloads).and_return([
+            "http://chromedriver.storage.googleapis.com/2.3/chromedriver_mac32.zip",
+            "http://chromedriver.storage.googleapis.com/2.4/chromedriver_mac32.zip",
+            "http://chromedriver.storage.googleapis.com/2.14/chromedriver_mac32.zip",
+            "http://chromedriver.storage.googleapis.com/2.2/chromedriver_mac32.zip",
+          ])
+      end
+
+      it "returns the newest version" do
+        expect(parser.newest_download).to eq("http://chromedriver.storage.googleapis.com/2.14/chromedriver_mac32.zip")
+      end
+    end
   end
 end
-
