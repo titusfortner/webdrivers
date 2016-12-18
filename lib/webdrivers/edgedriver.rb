2cca69f3a217c959b2553ec18fa5cfeb77f30428
Titus Fortner <titusfortner@gmail.com>
2016-12-18 16:48:52 -0600

    update to latest edgedriver version

--- lib/webdrivers/edgedriver.rb
+++ lib/webdrivers/edgedriver.rb
@@ -18,7 +18,7 @@ module Webdrivers
       end
 
       def download_url(version = nil)
-        if newest_version.to_i >= 14965
+        if newest_version.to_i >= 14986
           'https://download.microsoft.com/download/1/4/1/14156DA0-D40F-460A-B14D-1B264CA081A5/MicrosoftWebDriver.exe'
         else
           'https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe'
