4480a46113d22ed586616d62dc4350c65dfaccd3
Titus Fortner <titusfortner@gmail.com>
2017-08-18 19:00:18 -0500

    implement logger for webdrivers gem

--- lib/webdrivers.rb
+++ lib/webdrivers.rb
@@ -1,6 +1,13 @@
 require "webdrivers/selenium"
+require "webdrivers/logger"
 require "webdrivers/common"
 require "webdrivers/chromedriver"
 require "webdrivers/geckodriver"
 require "webdrivers/iedriver"
 require "webdrivers/mswebdriver"
+
+module Webdrivers
+  def self.logger
+    @logger ||= Webdrivers::Logger.new
+  end
+end
