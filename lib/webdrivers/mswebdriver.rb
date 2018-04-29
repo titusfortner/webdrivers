1c15d731e3e070ee16b8b019257931aa3bd38305
kapoorlakshya <kapoorlakshya@gmail.com>
2018-04-29 00:22:14 -0700

    MSWebdrivers now uses Edge version to download the correct binary version.
    Add spec to retrieve edge version.
    
    Reformat code.
    
    Remove Windows specific spec.

--- lib/webdrivers/mswebdriver.rb
+++ lib/webdrivers/mswebdriver.rb
@@ -4,51 +4,42 @@ module Webdrivers
 
       def current
         Webdrivers.logger.debug "Checking current version"
-        version = %x(ver)
-        Webdrivers.logger.debug "Current version of Windows Build is #{version}"
-        version[/\d+\.\d+\.\d+/][/[^\.]\d+$/]
-      end
 
-      def latest
-        # unknown; have to always download
+        # current() from other webdrivers returns the version from the webdriver EXE.
+        # Unfortunately, MicrosoftWebDriver.exe does not have an option to get the version.
+        # To work around it we query the currently installed version of Microsoft Edge instead
+        # and compare it to the list of available downloads.
+        version = `powershell (Get-AppxPackage -Name Microsoft.MicrosoftEdge).Version`
+        raise "Failed to check Microsoft Edge version." if version.empty? # Package name changed?
+        Webdrivers.logger.debug "Current version of Microsoft Edge is #{version.chomp!}"
+
+        build = version.split('.')[1] # "41.16299.248.0" => "16299"
+        Webdrivers.logger.debug "Expecting MicrosoftWebDriver.exe version #{build}"
+        build.to_i
       end
 
       private
 
       def normalize(string)
-        string.match(/(\d+)\.(\d+\.\d+)/).to_a.map {|v| v.tr('.', '')}[1..-1].join('.').to_f
+        string.match(/(\d+)\.(\d+\.\d+)/).to_a.map { |v| v.tr('.', '') }[1 .. -1].join('.').to_f
       end
 
       def file_name
         "MicrosoftWebDriver.exe"
       end
 
-      def download_url(_version = nil)
+      def downloads
         raise StandardError, "Can not reach site" unless site_available?
-
-        case current.to_i
-        when 10240
-          Webdrivers.logger.debug "Attempting to Download Build for 10240"
-          "https://download.microsoft.com/download/8/D/0/8D0D08CF-790D-4586-B726-C6469A9ED49C/MicrosoftWebDriver.msi"
-        when 10586
-          Webdrivers.logger.debug "Attempting to Download Build for 10586"
-          "https://download.microsoft.com/download/C/0/7/C07EBF21-5305-4EC8-83B1-A6FCC8F93F45/MicrosoftWebDriver.msi"
-        when 14393
-          Webdrivers.logger.debug "Attempting to Download Build for 14393"
-          "https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe"
-        when 15063
-          Webdrivers.logger.debug "Attempting to Download Build for 15063"
-          "https://download.microsoft.com/download/3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD/MicrosoftWebDriver.exe"
-        else
-          Webdrivers.logger.debug "Attempting to Download Latest Insider's Version"
-          "https://download.microsoft.com/download/1/4/1/14156DA0-D40F-460A-B14D-1B264CA081A5/MicrosoftWebDriver.exe"
+        array = Nokogiri::HTML(get(base_url)).xpath("//li[@class='driver-download']/a")
+        array.each_with_object({}) do |link, hash|
+          next if link.text == 'Insiders'
+          hash[link.text.scan(/\d+/).first.to_i] = link['href']
         end
       end
 
       def base_url
-        'https://www.microsoft.com/en-us/download'
+        'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/'
       end
-
     end
   end
 end
