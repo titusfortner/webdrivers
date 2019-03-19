79cd67e6891fe8a9a8de6edef6ad7f2bb9a3b950
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-03-19 13:44:02 -0700

    #latest_version now returns latest binary version corresponding to the installed Chrome version.

--- lib/webdrivers/chromedriver.rb
+++ lib/webdrivers/chromedriver.rb
@@ -14,7 +14,11 @@ module Webdrivers
 
       def latest_version
         raise StandardError, "Can not reach site" unless site_available?
-        Gem::Version.new(get(URI.join(base_url, "LATEST_RELEASE")))
+
+        # Latest webdriver release for installed Chrome version
+        release_file     = "LATEST_RELEASE_#{release_version}"
+        latest_available = get(URI.join(base_url, release_file))
+        Gem::Version.new(latest_available)
       end
 
       private
@@ -31,11 +35,11 @@ module Webdrivers
         Webdrivers.logger.debug "Versions previously located on downloads site: #{@downloads.keys}" if @downloads
 
         @downloads ||= begin
-          doc = Nokogiri::XML.parse(get(base_url))
+          doc   = Nokogiri::XML.parse(get(base_url))
           items = doc.css("Contents Key").collect(&:text)
-          items.select! {|item| item.include?(platform)}
+          items.select! { |item| item.include?(platform) }
           ds = items.each_with_object({}) do |item, hash|
-            key = normalize item[/^[^\/]+/]
+            key       = normalize item[/^[^\/]+/]
             hash[key] = "#{base_url}/#{item}"
           end
           Webdrivers.logger.debug "Versions now located on downloads site: #{ds.keys}"
@@ -43,6 +47,45 @@ module Webdrivers
         end
       end
 
+      # Returns release version from the currently installed Chrome version
+      #
+      # @example
+      #   73.0.3683.75 -> 73.0.3683
+      def release_version
+        chrome_version[/\d+\.\d+\.\d+/]
+      end
+
+      # Returns currently installed Chrome version
+      def chrome_version
+        ver = case platform
+                when 'win'
+                  chrome_on_windows
+                when /linux/
+                  chrome_on_linux
+                when 'mac'
+                  chrome_on_mac
+                else
+                  raise NotImplementedError, 'Your OS is not supported by webdrivers gem.'
+              end.chomp
+
+        # Google Chrome 73.0.3683.75 -> 73.0.3683.75
+        ver[/(\d|\.)+/]
+      end
+
+      def chrome_on_windows
+        reg = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
+        ps  = "(Get-Item (Get-ItemProperty '#{reg}').'(Default)').VersionInfo.ProductVersion"
+        `powershell #{ps}`
+      end
+
+      def chrome_on_linux
+        `google-chrome --version`
+      end
+
+      def chrome_on_mac
+        loc = Shellwords.escape '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        `#{loc} --version`
+      end
     end
   end
 end
