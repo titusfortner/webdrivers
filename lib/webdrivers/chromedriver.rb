928e69957680e42d1872f5eb87a0910a180e632f
Lakshya Kapoor <kapoorlakshya@gmail.com>
2019-03-25 13:45:35 -0700

    Check Selenium path for browser executable before checking for Google Chrome, and get full chromedriver version instead of just the major.

--- lib/webdrivers/chromedriver.rb
+++ lib/webdrivers/chromedriver.rb
@@ -5,19 +5,26 @@ module Webdrivers
     class << self
 
       def current_version
-        Webdrivers.logger.debug "Checking current version"
+        Webdrivers.logger.debug 'Checking current version'
         return nil unless downloaded?
-        string = %x(#{binary} --version)
-        Webdrivers.logger.debug "Current version of #{binary} is #{string}"
-        normalize string.match(/ChromeDriver (\d+\.\d+)/)[1]
+
+        ver = `#{binary} --version`
+        Webdrivers.logger.debug "Current #{binary} version: #{ver}"
+
+        # Matches 2.46, 2.46.628411 and 73.0.3683.75
+        normalize ver[/\d+\.\d+(\.\d+)?(\.\d+)?/]
       end
 
       def latest_version
-        raise StandardError, "Can not reach site" unless site_available?
+        raise StandardError, 'Can not reach site' unless site_available?
+
+        # Versions before 70 do not have a LATEST_RELEASE file
+        return Gem::Version.new('2.46') if release_version < '70.0.3538'
 
         # Latest webdriver release for installed Chrome version
         release_file     = "LATEST_RELEASE_#{release_version}"
         latest_available = get(URI.join(base_url, release_file))
+        Webdrivers.logger.debug "Latest version available: #{latest_available}"
         Gem::Version.new(latest_available)
       end
 
@@ -68,23 +75,58 @@ module Webdrivers
                   raise NotImplementedError, 'Your OS is not supported by webdrivers gem.'
               end.chomp
 
+        if ver.nil? || ver.empty?
+          raise StandardError, 'Failed to find Chrome binary or its version.'
+        end
+
         # Google Chrome 73.0.3683.75 -> 73.0.3683.75
         ver[/(\d|\.)+/]
       end
 
       def chrome_on_windows
-        reg = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
-        ps  = "(Get-Item (Get-ItemProperty '#{reg}').'(Default)').VersionInfo.ProductVersion"
-        `powershell #{ps}`
+        if browser_binary
+          Webdrivers.logger.debug "Browser executable: '#{browser_binary}'"
+          return `powershell (Get-ItemProperty '#{browser_binary}').VersionInfo.ProductVersion`
+        end
+
+        # Default to Google Chrome
+        reg        = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'
+        executable = `powershell (Get-ItemProperty '#{reg}' -Name '(default)').'(default)'`.strip
+        Webdrivers.logger.debug "Browser executable: '#{executable}'"
+        ps = "(Get-Item (Get-ItemProperty '#{reg}').'(default)').VersionInfo.ProductVersion"
+        `powershell #{ps}`.strip
       end
 
       def chrome_on_linux
-        `google-chrome --version`
+        if browser_binary
+          Webdrivers.logger.debug "Browser executable: '#{browser_binary}'"
+          return `#{browser_binary} --product-version`.strip
+        end
+
+        # Default to Google Chrome
+        executable = `which google-chrome`.strip
+        Webdrivers.logger.debug "Browser executable: '#{executable}'"
+        `#{executable} --product-version`.strip
       end
 
       def chrome_on_mac
-        loc = Shellwords.escape '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-        `#{loc} --version`
+        if browser_binary
+          Webdrivers.logger.debug "Browser executable: '#{browser_binary}'"
+          return `#{browser_binary} --version`.strip
+        end
+
+        # Default to Google Chrome
+        executable = Shellwords.escape '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        Webdrivers.logger.debug "Browser executable: #{executable}"
+        `#{executable} --version`.strip
+      end
+
+      #
+      # Returns user defined browser executable path from Selenium::WebDrivers::Chrome#path.
+      #
+      def browser_binary
+        # For Chromium, Brave, or whatever else
+        Selenium::WebDriver::Chrome.path
       end
     end
   end
