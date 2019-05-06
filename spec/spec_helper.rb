c1998574ec7a7c8b90fe16d2b8c7b5ea7856e121
Thomas Walpole <twalpole@gmail.com>
2019-05-06 12:07:58 -0700

    Add RSpec configuration to require `expect` syntax and enable partial double verification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,10 @@ require 'webdrivers'
 RSpec.configure do |config|
   config.filter_run_including focus: true unless ENV['CI']
   config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 end
