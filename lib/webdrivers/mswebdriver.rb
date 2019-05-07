948fd1ae25759274baa3b93d071471b3010204d6
Titus Fortner <titusfortner@gmail.com>
2019-05-07 12:17:51 -0500

    fix the monkey patch for > Selenium 3.141.0

--- lib/webdrivers/mswebdriver.rb
+++ lib/webdrivers/mswebdriver.rb
@@ -13,20 +13,41 @@ end
 module Selenium
   module WebDriver
     module Edge
-      class << self
-        alias se_driver_path driver_path
+      if Selenium::WebDriver::VERSION > '3.141.0'
+        class Service < WebDriver::Service
+          class << self
+            alias se_driver_path driver_path
 
-        def driver_path
-          unless Webdrivers::MSWebdriver.ignore
-            Webdrivers.logger.warn 'Microsoft WebDriver for the Edge browser is no longer supported by Webdrivers gem;'\
+            def driver_path
+              unless Webdrivers::MSWebdriver.ignore
+                Webdrivers.logger.warn 'Microsoft WebDriver for the Edge browser is no longer supported by Webdrivers gem;'\
           ' Due to changes in Edge implementation, the correct version can no longer be accurately provided. '\
           'Download driver, and specify the location with `Selenium::WebDriver::Edge.driver_path = "/driver/path"`, '\
           'or place it in PATH Environment Variable. '\
           'Download directions here: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/#downloads'\
           'To remove this warning in Webdrivers 3.x, set `Webdrivers.MSWebdriver.ignore`'
-          end
+              end
 
-          se_driver_path
+              se_driver_path
+            end
+          end
+        end
+      else
+        class << self
+          alias se_driver_path driver_path
+
+          def driver_path
+            unless Webdrivers::MSWebdriver.ignore
+              Webdrivers.logger.warn 'Microsoft WebDriver for the Edge browser is no longer supported by Webdrivers gem;'\
+          ' Due to changes in Edge implementation, the correct version can no longer be accurately provided. '\
+          'Download driver, and specify the location with `Selenium::WebDriver::Edge.driver_path = "/driver/path"`, '\
+          'or place it in PATH Environment Variable. '\
+          'Download directions here: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/#downloads'\
+          'To remove this warning in Webdrivers 3.x, set `Webdrivers.MSWebdriver.ignore`'
+            end
+
+            se_driver_path
+          end
         end
       end
     end
--- spec/webdrivers/ms_webdriver_spec.rb
+++ spec/webdrivers/ms_webdriver_spec.rb
@@ -6,17 +6,13 @@ describe Webdrivers::MSWebdriver do
   let(:mswebdriver) { described_class }
 
   it 'gives deprecation for using it' do
-    skip if Selenium::WebDriver::VERSION == '3.142.0'
-
-    service = instance_double(Selenium::WebDriver::Service, host: '', start: nil, uri: '')
-    bridge = instance_double(Selenium::WebDriver::Remote::Bridge, create_session: nil, session_id: '')
-
-    allow(Selenium::WebDriver::Service).to receive(:new).and_return(service)
-    allow(Selenium::WebDriver::Remote::Bridge).to receive(:new).and_return(bridge)
-    allow(Selenium::WebDriver::Remote::W3C::Bridge).to receive(:new)
-
     msg = /WARN Webdrivers Microsoft WebDriver for the Edge browser is no longer supported by Webdrivers gem/
-    expect { Selenium::WebDriver.for :edge }.to output(msg).to_stdout_from_any_process
+
+    if Selenium::WebDriver::VERSION > '3.141.0'
+      expect { Selenium::WebDriver::Edge::Service.driver_path }.to output(msg).to_stdout_from_any_process
+    else
+      expect { Selenium::WebDriver::Edge.driver_path }.to output(msg).to_stdout_from_any_process
+    end
   end
 
   it 'does not give deprecation when set to ignore' do
