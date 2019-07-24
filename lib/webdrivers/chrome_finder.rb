titusfortner/webdrivers

commit 61b9659c978bd73fb757e4b7153e45f7744392ac

Raise BrowserNotFound if Chrome and Edge binary are not found. (#146)
* Use openjdk8 with JRuby on Travis CI.

This is a workaround for jruby#5736 which is fixed in the next JRuby release - v9.2.8.0.

* Install microsoft-edge-dev instead of Canary on macOS (Travis CI).

* Raise BrowserNotFound if Chrome and Edge binary are not found.

lib/webdrivers/chrome_finder.rb
@@ -8,15 +8,17 @@ module Webdrivers
     class << self
       def version
         version = send("#{System.platform}_version", location)
-
-        raise VersionError, 'Failed to find Chrome binary or its version.' if version.nil? || version.empty?
+        raise VersionError, 'Failed to find Chrome version.' if version.nil? || version.empty?
 
         Webdrivers.logger.debug "Browser version: #{version}"
         version[/\d+\.\d+\.\d+\.\d+/] # Google Chrome 73.0.3683.75 -> 73.0.3683.75
       end
 
       def location
-        user_defined_location || send("#{System.platform}_location")
+        chrome_bin = user_defined_location || send("#{System.platform}_location")
+        return chrome_bin unless chrome_bin.nil?
+
+        raise BrowserNotFound, 'Failed to find Chrome binary.'
       end
 
       private

.travis.yml
@@ -24,6 +24,7 @@ matrix:
     - rvm: 2.4.6
       env: RAKE_TASK=rubocop
     - rvm: jruby-9.2.7.0
+      jdk: openjdk8
       env: RAKE_TASK=spec
     - rvm: 2.6
       env: RAKE_TASK=spec
@@ -35,6 +36,6 @@ matrix:
         homebrew:
           taps: homebrew/cask-versions
           casks:
-            - microsoft-edge-canary
+            - microsoft-edge-dev
   allow_failures:
     - gemfile: gemfiles/Gemfile.edge

lib/webdrivers/common.rb
@@ -18,6 +18,9 @@ module Webdrivers
   class NetworkError < StandardError
   end
 
+  class BrowserNotFound < StandardError
+  end
+
   DEFAULT_CACHE_TIME = 86_400 # 24 hours
   DEFAULT_INSTALL_DIR = File.expand_path(File.join(ENV['HOME'], '.webdrivers'))
 

lib/webdrivers/edge_finder.rb
@@ -8,15 +8,17 @@ module Webdrivers
     class << self
       def version
         version = send("#{System.platform}_version", location)
-
-        raise VersionError, 'Failed to find Edge binary or its version.' if version.nil? || version.empty?
+        raise VersionError, 'Failed to find Edge version.' if version.nil? || version.empty?
 
         Webdrivers.logger.debug "Browser version: #{version}"
         version[/\d+\.\d+\.\d+\.\d+/] # Microsoft Edge 73.0.3683.75 -> 73.0.3683.75
       end
 
       def location
-        user_defined_location || send("#{System.platform}_location")
+        edge_bin = user_defined_location || send("#{System.platform}_location")
+        return edge_bin unless edge_bin.nil?
+
+        raise BrowserNotFound, 'Failed to find Edge binary.'
       end
 
       private

spec/webdrivers/chrome_finder_spec.rb
@@ -7,38 +7,44 @@ describe Webdrivers::ChromeFinder do
 
   context 'when the user relies on the gem to figure out the location of Chrome' do
     it 'determines the location correctly based on the current OS' do
-      expect(chrome_finder.location).not_to be_nil
+      expect { chrome_finder.location }.not_to raise_error
     end
   end
 
   context 'when the user provides a path to the Chrome binary' do
     it 'uses Selenium::WebDriver::Chrome.path when it is defined' do
       Selenium::WebDriver::Chrome.path = chrome_finder.location
-      allow(chrome_finder).to receive(:win_location)
-      allow(chrome_finder).to receive(:mac_location)
-      allow(chrome_finder).to receive(:linux_location)
-      expect(chrome_finder.version).not_to be_nil
-      expect(chrome_finder).not_to have_received(:win_location)
-      expect(chrome_finder).not_to have_received(:mac_location)
-      expect(chrome_finder).not_to have_received(:linux_location)
-    end
+      locations = %i[win_location mac_location linux_location]
+      allow(chrome_finder).to receive_messages(locations)
 
-    it "uses ENV['WD_CHROME_PATH'] when it is defined" do
-      allow(ENV).to receive(:[]).with('WD_CHROME_PATH').and_return(chrome_finder.location)
-      allow(chrome_finder).to receive(:win_location)
-      allow(chrome_finder).to receive(:mac_location)
-      allow(chrome_finder).to receive(:linux_location)
       expect(chrome_finder.version).not_to be_nil
-      expect(chrome_finder).not_to have_received(:win_location)
-      expect(chrome_finder).not_to have_received(:mac_location)
-      expect(chrome_finder).not_to have_received(:linux_location)
+      locations.each { |loc| expect(chrome_finder).not_to have_received(loc) }
     end
+  end
 
-    it 'uses Selenium::WebDriver::Chrome.path over WD_CHROME_PATH' do
-      Selenium::WebDriver::Chrome.path = chrome_finder.location
-      allow(ENV).to receive(:[]).with('WD_CHROME_PATH').and_return('my_wd_chrome_path')
-      expect(chrome_finder.version).not_to be_nil
-      expect(ENV).not_to have_received(:[]).with('WD_CHROME_PATH')
+  it "uses ENV['WD_CHROME_PATH'] when it is defined" do
+    allow(ENV).to receive(:[]).with('WD_CHROME_PATH').and_return(chrome_finder.location)
+    locations = %i[win_location mac_location linux_location]
+    allow(chrome_finder).to receive_messages(locations)
+
+    expect(chrome_finder.version).not_to be_nil
+    locations.each { |loc| expect(chrome_finder).not_to have_received(loc) }
+  end
+
+  it 'uses Selenium::WebDriver::Chrome.path over WD_CHROME_PATH' do
+    Selenium::WebDriver::Chrome.path = chrome_finder.location
+    allow(ENV).to receive(:[]).with('WD_CHROME_PATH').and_return('my_wd_chrome_path')
+    expect(chrome_finder.version).not_to be_nil
+    expect(ENV).not_to have_received(:[]).with('WD_CHROME_PATH')
+  end
+
+  context 'when Chrome is not installed' do
+    it 'raises BrowserNotFound' do
+      locations = %i[win_location mac_location linux_location]
+      allow(chrome_finder).to receive_messages(locations)
+      allow(chrome_finder).to receive(:user_defined_location).and_return(nil)
+
+      expect { chrome_finder.version }.to raise_error(Webdrivers::BrowserNotFound)
     end
   end
 end

spec/webdrivers/edge_finder_spec.rb
@@ -1,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Webdrivers::EdgeFinder do
+  let(:edge_finder) { described_class }
+
+  before(:all) do # rubocop:disable RSpec/BeforeAfterAll
+    # Skip these tests if version of selenium-webdriver being tested with doesn't
+    # have Chromium based Edge support
+    unless defined?(Selenium::WebDriver::EdgeChrome)
+      skip "The current selenium-webdriver doesn't include Chromium based Edge support"
+    end
+  end
+
+  context 'when the user relies on the gem to figure out the location of Edge' do
+    it 'determines the location correctly based on the current OS' do
+      expect { edge_finder.location }.not_to raise_error
+    end
+  end
+
+  context 'when the user provides a path to the Edge binary' do
+    it 'uses Selenium::WebDriver::EdgeChrome.path when it is defined' do
+      Selenium::WebDriver::EdgeChrome.path = edge_finder.location
+      locations = %i[win_location mac_location linux_location]
+      allow(edge_finder).to receive_messages(locations)
+
+      expect(edge_finder.version).not_to be_nil
+      locations.each { |loc| expect(edge_finder).not_to have_received(loc) }
+    end
+
+    it "uses ENV['WD_EDGE_CHROME_PATH'] when it is defined" do
+      allow(ENV).to receive(:[]).with('WD_EDGE_CHROME_PATH').and_return(edge_finder.location)
+      locations = %i[win_location mac_location linux_location]
+      allow(edge_finder).to receive_messages(locations)
+
+      expect(edge_finder.version).not_to be_nil
+      locations.each { |loc| expect(edge_finder).not_to have_received(loc) }
+    end
+
+    it 'uses Selenium::WebDriver::EdgeChrome.path over WD_EDGE_CHROME_PATH' do
+      Selenium::WebDriver::EdgeChrome.path = edge_finder.location
+      allow(ENV).to receive(:[]).with('WD_EDGE_CHROME_PATH').and_return('my_wd_chrome_path')
+      expect(edge_finder.version).not_to be_nil
+      expect(ENV).not_to have_received(:[]).with('WD_EDGE_CHROME_PATH')
+    end
+  end
+
+  context 'when Edge is not installed' do
+    it 'raises BrowserNotFound' do
+      locations = %i[win_location mac_location linux_location]
+      allow(edge_finder).to receive_messages(locations)
+      allow(edge_finder).to receive(:user_defined_location).and_return(nil)
+
+      expect { edge_finder.version }.to raise_error(Webdrivers::BrowserNotFound)
+    end
+  end
+end

commit 210d5dc185f70aaefe6a3444d32c0a3c48bd0c0d

Fix a bug introduced by #146 in ChromeFinder.
Re-adding a fix which I had accidentally reverted during the final force push. The fix updates the location methods in the ChromeFinder to return nil if a valid binary is not found.

lib/webdrivers/chrome_finder.rb
@@ -46,6 +46,8 @@ module Webdrivers
             return option if File.exist?(option)
           end
         end
+
+        nil
       end
 
       def mac_location
@@ -59,6 +61,8 @@ module Webdrivers
             return option if File.exist?(option)
           end
         end
+
+        nil
       end
 
       def linux_location
@@ -71,6 +75,8 @@ module Webdrivers
             return option if File.exist?(option)
           end
         end
+
+        nil
       end
 
       def win_version(location)